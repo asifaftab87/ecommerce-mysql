4ee149628e430f7b0f33f719b50fe2aec950ea2a
asifaftab87 <asifaftab87@gmail.com>
2020-11-19 09:26:22 +0800

    adding user files

--- src/main/java/org/la/ecom/msql/rest/controller/UserRestController.java
+++ src/main/java/org/la/ecom/msql/rest/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package org.la.ecom.msql.rest.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.dozer.DozerBeanMapper;
@@ -10,14 +11,17 @@ import org.la.ecom.msql.service.UserService;
 import org.la.ecom.mysql.api.AppConstants;
 import org.la.ecom.mysql.api.dto.MailFormatDTO;
 import org.la.ecom.mysql.api.dto.MailFormatEmailListDTO;
+import org.la.ecom.mysql.api.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequestMapping("/user")
 public class UserRestController {
 
 	@Autowired
@@ -32,16 +36,21 @@ public class UserRestController {
 	@GetMapping("/allMailsToSend/{mailSent}")
 	public MailFormatEmailListDTO getAllMailsToSend(@PathVariable int mailSent){
 		
-		List<String> mailList = userService.getAllEmailsToSend(mailSent);
+		List<User> userList = userService.findByMailSentStatus(mailSent);
+		
 		MailFormatEmailListDTO mailFormatEmailListDTO = null;
 		
-		if(mailList!=null && !mailList.isEmpty()) {
+		if(userList!=null && !userList.isEmpty()) {
+			
+			List<UserDTO> userDTOList = new ArrayList<>();
+			
+			userList.stream().forEach(user -> userDTOList.add(mapper.map(user, UserDTO.class)));
 			
 			mailFormatEmailListDTO = new MailFormatEmailListDTO();
 			MailFormat mailFormat = mailFormatService.findByMailTypeId(AppConstants.MAIL_SEND_ON_REGISTRATION);
 			
 			if(mailFormat!=null) {
-				mailFormatEmailListDTO.setMailList(mailList);
+				mailFormatEmailListDTO.setUserList(userDTOList);
 				mailFormatEmailListDTO.setMailFormatDTO(mapper.map(mailFormat, MailFormatDTO.class));
 			}
 		}
--- src/main/java/org/la/ecom/msql/service/UserService.java
+++ src/main/java/org/la/ecom/msql/service/UserService.java
@@ -16,60 +16,65 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private RoleService roleService;
-	
+
 	@Autowired
 	private DozerBeanMapper dozerBeanMapper;
-	
-	public List<String> getAllEmailsToSend(int mailSent){
-		
+
+	public List<String> getAllEmailsToSend(int mailSent) {
+
 		List<String> emailList = new ArrayList<>();
-		
+
 		List<User> userList = userRepository.findByMailSentStatus(mailSent);
-		
-		if(userList!=null && !userList.isEmpty()) {
+
+		if (userList != null && !userList.isEmpty()) {
 			userList.stream().forEach(user -> emailList.add(user.getEmail()));
 		}
-		
+
 		return emailList;
 	}
-	
+
+	public List<User> findByMailSentStatus(int mailSent) {
+
+		List<User> userList = userRepository.findByMailSentStatus(mailSent);
+
+		return userList;
+	}
+
 	public List<User> getAllUser() {
 		return userRepository.findAll();
 	}
 
 	public User findByEmail(String email) {
-		
+
 		Optional<User> user = userRepository.findByEmail(email);
-		
+
 		if (user.isPresent()) {
 			return user.get();
 		}
 		return null;
 	}
-	
-	public Collection<User> findAllUsersExceptGivenEmail(String email){
+
+	public Collection<User> findAllUsersExceptGivenEmail(String email) {
 		return userRepository.findAllUsersExceptGivenEmail(email);
 	}
-	
-	
+
 	public User findById(long id) {
-		
+
 		Optional<User> optional = userRepository.findById(id);
-		
-		if(optional.isPresent()) {
-			
+
+		if (optional.isPresent()) {
+
 			User user = optional.get();
-			
+
 			return user;
 		}
-		
+
 		return null;
 	}
-	
-	
+
 	public void update(User user) {
 		userRepository.saveAndFlush(user);
 	}
